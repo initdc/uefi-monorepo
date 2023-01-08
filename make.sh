e96999952d16b195e6668bed4740d4bb27744324
initdc <initd@outlook.com>
2023-01-08 12:30:58 +0000

    feat: adapt to NanoPi R6S

--- make.sh
+++ make.sh
@@ -22,7 +22,6 @@ CMD=`realpath $0`
 COMMON_DIR=`dirname $CMD`
 TOP_DIR=$(realpath $COMMON_DIR/..)
 echo $TOP_DIR
-export GCC5_AARCH64_PREFIX=$TOP_DIR/prebuilts/gcc/linux-x86/aarch64/gcc-arm-10.3-2021.07-x86_64-aarch64-none-linux-gnu/bin/aarch64-none-linux-gnu-
 export WORKSPACE=$COMMON_DIR
 export PACKAGES_PATH=$WORKSPACE/edk2:$WORKSPACE/edk2-platforms:$WORKSPACE/edk2-non-osi
 . edk2/edksetup.sh BaseTools &&
@@ -31,7 +30,7 @@ build -a AARCH64 -t GCC5 -p edk2-platforms/Platform/Rockchip/RK$CHIP/RK$CHIP.dsc
 echo "cd u-boot and re-packet uboot.img: ./scripts/fit-repack.sh -f uboot.img -d unpack/" &&
 cd ../u-boot &&
 mkdir -p unpack &&
-./make.sh rk$CHIP CROSS_COMPILE=$TOP_DIR/prebuilts/gcc/linux-x86/aarch64/gcc-arm-10.3-2021.07-x86_64-aarch64-none-linux-gnu/bin/aarch64-none-linux-gnu- &&
+./make.sh nanopi6 &&
 ./make.sh --idblock
 cp uboot.img uboot_uefi.img
 cp $WORKSPACE/Build/RK$CHIP/DEBUG_GCC5/FV/BL33_AP_UEFI.Fv unpack/uboot &&
